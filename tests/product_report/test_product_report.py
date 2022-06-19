fc93bfeb94f2a9f2b2a29a1ba91f9ebd9c393154
natferlima <ferlima.natalia@gmail.com>
2022-06-19 19:01:49 -0300

    feat: testa o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "produto1",
+        "empresa1",
+        "19/06/2022",
+        "19/06/2024",
+        123456789,
+        "ao abrigo de luz")
+
+    expected = (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+
+    assert str(product) == expected
