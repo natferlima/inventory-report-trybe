87d08c7abab581ccafc4618c3087321eddaddda6
natferlima <ferlima.natalia@gmail.com>
2022-06-19 18:50:46 -0300

    feat: testar o inicializador/construtor do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "produto1",
+        "empresa1",
+        "19/06/2022",
+        "19/06/2024",
+        123456789,
+        "instuções de armazenamento")
+
+    assert product.id == 1
+    assert product.nome_do_produto == "produto1"
+    assert product.nome_da_empresa == "empresa1"
+    assert product.data_de_fabricacao == "19/06/2022"
+    assert product.data_de_validade == "19/06/2024"
+    assert product.numero_de_serie == 123456789
+    assert product.instrucoes_de_armazenamento == "instuções de armazenamento"
