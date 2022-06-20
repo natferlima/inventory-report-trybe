6600d960080cf47c049af6a1ebfd34fc0a612fcc
natferlima <ferlima.natalia@gmail.com>
2022-06-19 21:27:55 -0300

    feat: gera os relatórios através de um arquivo json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,15 +1,19 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
-    def import_data(path, report_type):
+    def import_data(path_file, report_type):
         data_list = []
-        with open(path, 'r') as file:
-            csv_file = csv.DictReader(file)
-            for row in csv_file:
-                data_list.append(dict(row))
+        with open(path_file, 'r') as file:
+            if path_file.endswith(".csv"):
+                csv_file = csv.DictReader(file)
+                for row in csv_file:
+                    data_list.append(dict(row))
+            if path_file.endswith(".json"):
+                data_list = json.load(file)
 
         if report_type == "simples":
             return SimpleReport.generate(data_list)
