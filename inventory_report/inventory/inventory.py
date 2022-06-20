f9421bec78d89b4dea83969978e046e13b040402
natferlima <ferlima.natalia@gmail.com>
2022-06-19 21:37:55 -0300

    feat: ajustes na classe Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,15 +5,25 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
+    def import_data_csv(data_list, path_file):
+        with open(path_file, 'r') as file:
+            csv_file = csv.DictReader(file)
+            for row in csv_file:
+                data_list.append(dict(row))
+            return data_list
+
+    def import_data_json(data_list, path_file):
+        with open(path_file, 'r') as file:
+            data_list = json.load(file)
+            return data_list
+
     def import_data(path_file, report_type):
         data_list = []
-        with open(path_file, 'r') as file:
-            if path_file.endswith(".csv"):
-                csv_file = csv.DictReader(file)
-                for row in csv_file:
-                    data_list.append(dict(row))
-            if path_file.endswith(".json"):
-                data_list = json.load(file)
+        # https://www.programiz.com/python-programming/methods/string/endswith
+        if path_file.endswith(".csv"):
+            data_list = Inventory.import_data_csv(data_list, path_file)
+        if path_file.endswith(".json"):
+            data_list = Inventory.import_data_json(data_list, path_file)
 
         if report_type == "simples":
             return SimpleReport.generate(data_list)
